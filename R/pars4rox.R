09e0afae5e498231474c2a28416b3cc48deb9010
tyler rinker <tyler_rinker@hotmail.com>
2012-10-16 01:40:26 -0400

    single space

--- R/pars4rox.R
+++ R/pars4rox.R
@@ -1,7 +1,7 @@
 pars4rox <-
 function(pars, dots = FALSE) {
     require(qdap)
-    x <- paste0("#' @param ", spaste(qdap::qcv(terms=pars, split = ",")))
+    x <- paste0("#' @param", spaste(qdap::qcv(terms=pars, split = ",")))
     x <- qdap::Trim(sapply(strsplit(x, "="), function(x) x[1]))
     zz <- matrix(rbind(x), ncol=1)
     if (dots) {
