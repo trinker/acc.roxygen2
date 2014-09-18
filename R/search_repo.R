51fe097a8916876300f98c4c899fbe7d381e6ee9
trinker <tyler.rinker@gmail.com>
2014-09-17 22:32:22 -0400

    updated search_repo, more efficient and flexible

--- R/search_repo.R
+++ R/search_repo.R
@@ -10,33 +10,31 @@
 #' are the term separators).
 #' @param split Character vector of length one to use for splitting (i.e. the 
 #' separator used in the vector).  For use with the argument \code{terms}.
-#' @param char.keep A character vector of symbol character (i.e. punctuation) 
-#' that strip should keep.  The default is to strip everything except 
-#' apostrophes. \code{\link[qdap]{termco}} attempts to auto detect characters to 
-#' keep based on the elements in \code{match.list}. 
-#' @importFrom qdap scrubber termco left_just
+#' @param ignore.case logical.  If \code{FALSE}, the pattern matching is case 
+#' sensitive and if \code{TRUE}, case is ignored during matching.
+#' @param perl logical. Should Perl-compatible regexps be used?
+#' @param fixed logical. If \code{TRUE}, pattern is a string to be matched as 
+#' is. Overrides all conflicting arguments.
+#' @importFrom qdap left_just
 #' @export
-search_repo <- function(..., repo = getOption("primary_repo"), 
-    base.git = getOption("base_git"), terms = NULL, split = "\\|", 
-    char.keep = NULL) {
-    require(qdap)
+search_repo <- function (..., repo = getOption("primary_repo"), base.git = getOption("base_git"), 
+    terms = NULL, split = "\\|", ignore.case = TRUE, perl = FALSE, fixed = FALSE) {
+
     path <- file.path(base.git, repo, "R")
     if (!is.null(terms)) {
-        x <- strsplit(terms, split = split)
+        terms <- strsplit(terms, split = split)
     } else {
-        x <- substitute(...())
+        terms <- substitute(...())
     }
-    unblanker <- function(x)subset(x, nchar(x)>0)
-    z <- unblanker(scrubber(unlist(lapply(x, function(y) as.character(y)))))
-    z <- unblanker(z)
-    WD <- getwd()
-    setwd(path)
-    m <- suppressWarnings(sapply(dir(), readLines))
-    n <- do.call(rbind, lapply(seq_along(m), function(i){
-        data.frame(fun=rep(names(m)[i], length(m[[i]])), text=m[[i]])
-    }))
-    x <- with(n, termco(text, fun, z, char.keep = char.keep))
-    y <- x$raw[rowSums(x$raw[, -c(1:2), drop=FALSE]) !=0, ]
-    setwd(WD)
-    left_just(replacer(y), 1)[, -2]
+    m <- setNames(suppressWarnings(sapply(file.path(path, dir(path)), readLines)), dir(path))
+
+    out2 <- setNames(lapply(terms, function(x) {
+        out <- lapply(m, function(y = m, z = x) {
+            grep(x, y, ignore.case = ignore.case, perl = perl, fixed = fixed)
+        })
+        out <- sapply(out[!sapply(out, identical, integer(0))], paste, collapse=", ")
+        data.frame(file=names(out), row.number=out, row.names=NULL)
+    }), unlist(terms))
+
+    lapply(out2, qdap::left_just)
 }
